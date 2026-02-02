fd6d68480dca93505a00e25de2c447c6d49b64c7
Marco Pragliola <marcopragliola@gmail.com>
2026-02-02 17:30:30 +0100

    Update Mooer device dimensions and image references in data file for consistency and accuracy.

--- src/data/devices-brands/mooer.ts
+++ src/data/devices-brands/mooer.ts
@@ -7,7 +7,7 @@ const WDH_MOOER_GE200: [number, number, number] = [297, 174, 45.5];
 const WDH_MOOER_GE200_PLUS: [number, number, number] = [297, 174, 45.5];
 const WDH_MOOER_GE250: [number, number, number] = [324, 170, 60];
 const WDH_MOOER_GE1000: [number, number, number] = [337, 170, 53];
-const WDH_MOOER_EXPLINE: [number, number, number] = [55, 128, 58];
+const WDH_MOOER_EXPLINE: [number, number, number] = [55, 92, 58];
 const WDH_OCEAN_MACHINE_II: [number, number, number] = [215, 110, 45];
 const WDH_MACRO_POWER_S8: [number, number, number] = [165, 83, 52];
 const WDH_MVP: [number, number, number] = [86.6, 125, 60];
@@ -16,11 +16,11 @@ const WDH_MICRO_MINI: [number, number, number] = [93.5, 42, 52];
 
 const mooerDevices: Omit<DeviceTemplate, "id" | "name" | "brand">[] = [
   { type: "power unit", model: "Macro Power S8", wdh: WDH_MACRO_POWER_S8, image: null },
-  { type: "multifx", model: "GE100", wdh: WDH_MOOER_GE100, image: "ge-100.png" },
-  { type: "multifx", model: "GE150 Plus Li", wdh: WDH_MOOER_GE150_PLUS_LI, image: "ge150plusli.png" },
+  { type: "multifx", model: "GE100", wdh: WDH_MOOER_GE100, image: "mooer-ge-100.png" },
+  { type: "multifx", model: "GE150 Plus Li", wdh: WDH_MOOER_GE150_PLUS_LI, image: "mooer-ge150plusli.png" },
   { type: "multifx", model: "GE200", wdh: WDH_MOOER_GE200, image: null },
-  { type: "multifx", model: "GE200 Plus", wdh: WDH_MOOER_GE200_PLUS, image: "ge200plus.png" },
-  { type: "multifx", model: "GE250", wdh: WDH_MOOER_GE250, image: "ge-250.png" },
+  { type: "multifx", model: "GE200 Plus", wdh: WDH_MOOER_GE200_PLUS, image: "mooer-ge200plus.png" },
+  { type: "multifx", model: "GE250", wdh: WDH_MOOER_GE250, image: "mooer-ge-250.png" },
   { type: "multifx", model: "GE1000", wdh: WDH_MOOER_GE1000, image: "ge1000.png" },
   { type: "pedal", model: "Expline", wdh: WDH_MOOER_EXPLINE, image: "mooer-expline.png" },
   { type: "pedal", model: "GL100", wdh: WDH_MICRO_MINI, image: "gl100.png" },
