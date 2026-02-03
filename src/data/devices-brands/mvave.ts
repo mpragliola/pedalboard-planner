51f1654beca00d4aea04e8f74ad8008e4064f03d
Marco Pragliola <marcopragliola@gmail.com>
2026-02-03 23:12:10 +0100

    Update Mvave device templates with new dimensions and types
    - Replaced existing compact pedal dimensions with specific Wdh values for new models.
    - Updated device types for Blackbox and Chocolate models to 'multifx' and 'controller' respectively.
    - Added new device entries for Cuvave Acoustic and Tank G with appropriate dimensions and images.

--- src/data/devices-brands/mvave.ts
+++ src/data/devices-brands/mvave.ts
@@ -1,16 +1,20 @@
 import type { DeviceTemplate } from "../devices";
 import { deviceId } from "../../lib/slug";
+import { Wdh } from "../../wdh";
 
 // Compact pedal size [width, depth, height] mm
-const WDH_COMPACT: [number, number, number] = [73, 129, 59];
+const WDH_CUVAVE: Wdh = [162, 61, 23];
+const WDH_CHOCOLATE: Wdh = [212, 36, 17];
+const WDH_TANKG: Wdh = [230, 100, 35];
+const WHD_BLACKBOX: Wdh = [110, 110, 35];
 
 const mvaveDevices: Omit<DeviceTemplate, "id" | "name" | "brand">[] = [
-  { type: "pedal", model: "Blackbox", wdh: WDH_COMPACT, image: "mvave-blackbox.png" },
-  { type: "pedal", model: "Chocolate", wdh: WDH_COMPACT, image: "mvave-chocolate.png" },
-  { type: "pedal", model: "Chocolate Plus", wdh: WDH_COMPACT, image: "mvave-chocolate-plus.png" },
-  { type: "pedal", model: "Cucave Acoustic", wdh: WDH_COMPACT, image: "mvave-cucave-acoustic.png" },
-  { type: "pedal", model: "Cuvave", wdh: WDH_COMPACT, image: "mvave-cuvave.png" },
-  { type: "pedal", model: "GTank", wdh: WDH_COMPACT, image: "mvave-gtank.png" },
+  { type: "multifx", model: "Blackbox", wdh: WHD_BLACKBOX, image: "mvave-blackbox.png" },
+  { type: "controller", model: "Chocolate", wdh: WDH_CHOCOLATE, image: "mvave-chocolate.png" },
+  { type: "controller", model: "Chocolate Plus", wdh: WDH_CHOCOLATE, image: "mvave-chocolate-plus.png" },
+  { type: "multifx", model: "Cuvave Acoustic", wdh: WDH_CUVAVE, image: "mvave-cucave-acoustic.png" },
+  { type: "multifx", model: "Cuvave (Cube Baby)", wdh: WDH_CUVAVE, image: "mvave-cuvave.png" },
+  { type: "multifx", model: "Tank G", wdh: WDH_TANKG, image: "mvave-gtank.png" },
 ];
 
 export const MVAVE_DEVICE_TEMPLATES: DeviceTemplate[] = mvaveDevices.map((d) => ({
