5ad44fe4a366e7c0a50988080a261a6ad96da71c
Marco Pragliola <marcopragliola@gmail.com>
2026-02-06 15:04:34 +0100

    Update Adam and Blackbird boards

--- src/data/boards-brands/aclam.ts
+++ src/data/boards-brands/aclam.ts
@@ -1,7 +1,6 @@
 import type { BoardTemplate } from '../boards'
 
 export const ACLAM_BOARD_TEMPLATES: BoardTemplate[] = [
-  { id: 'board-aclam-xs1', type: 'classic', brand: 'Aclam', model: 'Smart Track XS1', name: 'Aclam Smart Track XS1', wdh: [420, 125, 25], image: 'aclam/aclam-smart-track-xs1.png' },
   { id: 'board-aclam-xs2', type: 'classic', brand: 'Aclam', model: 'Smart Track XS2', name: 'Aclam Smart Track XS2', wdh: [420, 250, 25], image: 'aclam/aclam-smart-track-xs2.png' },
   { id: 'board-aclam-s1', type: 'classic', brand: 'Aclam', model: 'Smart Track S1', name: 'Aclam Smart Track S1', wdh: [590, 125, 25], image: 'aclam/aclam-smart-track-s1.png' },
   { id: 'board-aclam-s2', type: 'classic', brand: 'Aclam', model: 'Smart Track S2', name: 'Aclam Smart Track S2', wdh: [590, 250, 25], image: 'aclam/aclam-smart-track-s2.png' },
--- src/data/boards-brands/blackbird.ts
+++ src/data/boards-brands/blackbird.ts
@@ -2,9 +2,6 @@ import { WDH_BOARD_457_305_76, WDH_BOARD_610_381_76, WDH_BOARD_813_406_76 } from
 import type { BoardTemplate } from '../boards'
 
 export const BLACKBIRD_BOARD_TEMPLATES: BoardTemplate[] = [
-  { id: 'board-blackbird-standard', type: 'classic', brand: 'Blackbird', model: 'Standard', name: 'Blackbird Standard', wdh: WDH_BOARD_457_305_76, image: null },
-  { id: 'board-blackbird-custom-24', type: 'classic', brand: 'Blackbird', model: 'Custom 24', name: 'Blackbird Custom 24', wdh: WDH_BOARD_610_381_76, image: null },
-  { id: 'board-blackbird-custom-32', type: 'classic', brand: 'Blackbird', model: 'Custom 32', name: 'Blackbird Custom 32', wdh: WDH_BOARD_813_406_76, image: null },
   { id: 'board-blackbird-feather-board', type: 'classic', brand: 'Blackbird', model: 'Feather Board', name: 'Blackbird Feather Board', wdh: WDH_BOARD_457_305_76, image: 'blackbird/blackbird-feather-board-top.png' },
   { id: 'board-blackbird-1630hc', type: 'classic', brand: 'Blackbird', model: '1630HC', name: 'Blackbird 1630HC', wdh: WDH_BOARD_457_305_76, image: 'blackbird/blackbird-1630hc.png' },
 ]
