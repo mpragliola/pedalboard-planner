887d5700b0879145fbdd05b6828efa7c979cdb85
Marco <marco@DESKTOP-IQQUOHB.localdomain>
2026-02-18 19:49:42 +0100

    Deploy pages to a side branch

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const basePath = process.env.VITE_BASE_PATH || '/pedalboard-planner/'
+
 export default defineConfig({
   plugins: [react({
     include: /\.(jsx|tsx)$/,
   })],
-  base: "/pedalboard-planner/",
+  base: basePath,
   test: {
     globals: true,
     environment: 'jsdom',
