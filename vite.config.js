dc230932c401c1bb1c04b0bb4d6bf1151493efb0
Marco <marco@DESKTOP-IQQUOHB.localdomain>
2026-02-23 01:41:33 +0100

    Patch vite.config.js. This removes the deprecation warnings, but it's convenient to upgrade Vite later. A comment and a @TODO will keep track of this.

--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,15 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
     setupFiles: ['src/test/setup.ts'],
   },
+  // @TODO: Remove this once the new SCSS compiler is stable and 
+  // supports all features we use. Might need upgrading Vite
+  // (>=5.4, >=6.0). This removes all the annoying deprecation
+  // warnings about the old compile during the build.
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler", // or "modern"
+      },
+    },
+  },
 })
